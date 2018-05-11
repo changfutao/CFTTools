93c9f1d3102ea4f0566372c59fd7af2cf0e7e0d3
Administrator <Administrator@MicroWin10-1159>
2018-05-11 16:47:50 +0800

    添加项目文件。

---
+++ CFTTools/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace CFTTools
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnCreateSQLType = new System.Windows.Forms.Button();
+            this.txtFrom = new System.Windows.Forms.TextBox();
+            this.txtTo = new System.Windows.Forms.TextBox();
+            this.btnCreateDbLink = new System.Windows.Forms.Button();
+            this.btnCreateToUpper = new System.Windows.Forms.Button();
+            this.btnArrange = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnCreateSQLType
+            // 
+            this.btnCreateSQLType.Location = new System.Drawing.Point(225, 12);
+            this.btnCreateSQLType.Name = "btnCreateSQLType";
+            this.btnCreateSQLType.Size = new System.Drawing.Size(111, 36);
+            this.btnCreateSQLType.TabIndex = 2;
+            this.btnCreateSQLType.Text = "SQL格式=>";
+            this.btnCreateSQLType.UseVisualStyleBackColor = true;
+            this.btnCreateSQLType.Click += new System.EventHandler(this.btnCreateSQLType_Click);
+            // 
+            // txtFrom
+            // 
+            this.txtFrom.Location = new System.Drawing.Point(13, 12);
+            this.txtFrom.Multiline = true;
+            this.txtFrom.Name = "txtFrom";
+            this.txtFrom.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtFrom.Size = new System.Drawing.Size(206, 434);
+            this.txtFrom.TabIndex = 3;
+            // 
+            // txtTo
+            // 
+            this.txtTo.Location = new System.Drawing.Point(342, 12);
+            this.txtTo.Multiline = true;
+            this.txtTo.Name = "txtTo";
+            this.txtTo.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtTo.Size = new System.Drawing.Size(206, 434);
+            this.txtTo.TabIndex = 4;
+            // 
+            // btnCreateDbLink
+            // 
+            this.btnCreateDbLink.Location = new System.Drawing.Point(225, 54);
+            this.btnCreateDbLink.Name = "btnCreateDbLink";
+            this.btnCreateDbLink.Size = new System.Drawing.Size(111, 36);
+            this.btnCreateDbLink.TabIndex = 5;
+            this.btnCreateDbLink.Text = "DBLink格式=>";
+            this.btnCreateDbLink.UseVisualStyleBackColor = true;
+            this.btnCreateDbLink.Click += new System.EventHandler(this.btnCreateDbLink_Click);
+            // 
+            // btnCreateToUpper
+            // 
+            this.btnCreateToUpper.Location = new System.Drawing.Point(225, 96);
+            this.btnCreateToUpper.Name = "btnCreateToUpper";
+            this.btnCreateToUpper.Size = new System.Drawing.Size(111, 36);
+            this.btnCreateToUpper.TabIndex = 6;
+            this.btnCreateToUpper.Text = "大写=>";
+            this.btnCreateToUpper.UseVisualStyleBackColor = true;
+            this.btnCreateToUpper.Click += new System.EventHandler(this.btnCreateToUpper_Click);
+            // 
+            // btnArrange
+            // 
+            this.btnArrange.Location = new System.Drawing.Point(225, 138);
+            this.btnArrange.Name = "btnArrange";
+            this.btnArrange.Size = new System.Drawing.Size(111, 36);
+            this.btnArrange.TabIndex = 7;
+            this.btnArrange.Text = "整理=>";
+            this.btnArrange.UseVisualStyleBackColor = true;
+            this.btnArrange.Click += new System.EventHandler(this.btnArrange_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(570, 450);
+            this.Controls.Add(this.btnArrange);
+            this.Controls.Add(this.btnCreateToUpper);
+            this.Controls.Add(this.btnCreateDbLink);
+            this.Controls.Add(this.txtTo);
+            this.Controls.Add(this.txtFrom);
+            this.Controls.Add(this.btnCreateSQLType);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "编程小帮手";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnCreateSQLType;
+        private System.Windows.Forms.TextBox txtFrom;
+        private System.Windows.Forms.TextBox txtTo;
+        private System.Windows.Forms.Button btnCreateDbLink;
+        private System.Windows.Forms.Button btnCreateToUpper;
+        private System.Windows.Forms.Button btnArrange;
+    }
+}
+
---
+++ CFTTools/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CFTTools
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        /// <summary>
+        /// 生成SQL格式字符串
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnCreateSQLType_Click(object sender, EventArgs e)
+        {
+            //生成前字符串
+            string listFrom = txtFrom.Text;
+            if (!string.IsNullOrEmpty(listFrom))
+            {
+                var strList = listFrom.Split(new char[] { '\r', '\n', ' ', ',', '\t' }, StringSplitOptions.RemoveEmptyEntries).ToList();
+                StringBuilder sbJoin = new StringBuilder();
+                strList.ForEach(item =>
+                {
+                    sbJoin.Append("'" + item + "',");
+                });
+                txtTo.Text = sbJoin.ToString().Substring(0, sbJoin.ToString().Length - 1);
+            }
+            else
+            {
+                MessageBox.Show("请输入要生成的内容!");
+            }
+
+        }
+        /// <summary>
+        /// 生成DBLink格式
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnCreateDbLink_Click(object sender, EventArgs e)
+        {
+            //生成前字符串
+            string listFrom = txtFrom.Text;
+            if (!string.IsNullOrEmpty(listFrom))
+            {
+                var strList = listFrom.Split(new char[] { '\r', '\n', ' ', ',', '\t' }, StringSplitOptions.RemoveEmptyEntries).ToList();
+                StringBuilder sbJoin = new StringBuilder();
+                strList.ForEach(item =>
+                {
+                    sbJoin.Append("''" + item + "'',");
+                });
+                txtTo.Text = sbJoin.ToString().Substring(0, sbJoin.ToString().Length - 1);
+            }
+            else
+            {
+                MessageBox.Show("请输入要生成的内容!");
+            }
+        }
+        /// <summary>
+        /// 生成大写格式
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnCreateToUpper_Click(object sender, EventArgs e)
+        {
+            //生成前字符串
+            string listFrom = txtFrom.Text;
+            if (!string.IsNullOrEmpty(listFrom))
+            {
+                txtTo.Text = listFrom.ToUpper();
+            }
+            else
+            {
+                MessageBox.Show("请输入要生成的内容!");
+            }
+        }
+
+        private void btnArrange_Click(object sender, EventArgs e)
+        {
+            string listFrom = txtFrom.Text;
+            List<string> listFroms=listFrom.Split(new char[] { ',', ' ', '\t', '\r', '\n' },StringSplitOptions.RemoveEmptyEntries).ToList();
+            StringBuilder sbTos = new StringBuilder();
+            foreach (var lf in listFroms)
+            {
+                sbTos.Append(lf+"\r\n");
+            }
+            txtTo.Text = sbTos.ToString();
+        }
+    }
+}
